61dc134c91fe3e59dc38a7c5f404b88041b50b6e
Kevin Chow <chowmein113@berkeley.edu>
2023-01-21 23:13:43 -0800

    Finished problem 1 and started 2

--- hw1/data_partitioning.py
+++ hw1/data_partitioning.py
@@ -7,6 +7,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import svm
 from scipy import io
+from threading import Thread
 
 data_sets = {}
 fields = "test_data", "training_data", "training_labels"
@@ -15,8 +16,36 @@ for data_name in ["mnist", "spam", "cifar10"]:
     data_sets[data_name] = np.load(f"./data/{data_name}-data.npz")
     print("\nloaded %s data!" % data_name)
     
-    
-
+def make_validation_mnist(key_arr):
+    global data
+    global valid_mnist_labels
+    global valid_mnist_data
+    labels = data.get("training_labels")
+    train_data = data.get("training_data")
+    for key in key_arr:
+        valid_mnist_labels.append(labels[key])
+        valid_mnist_data.append(train_data[key])
+    print("finished process")
+def make_validation_spam(key_arr):
+    global data
+    global valid_spam_labels
+    global valid_spam_data
+    labels = data.get("training_labels")
+    train_data = data.get("training_data")
+    for key in key_arr:
+        valid_spam_labels.append(labels[key])
+        valid_spam_data.append(train_data[key])
+    print("finished process")
+def make_validation_cfar(key_arr):
+    global data
+    global valid_cfar_labels
+    global valid_cfar_data
+    labels = data.get("training_labels")
+    train_data = data.get("training_data")
+    for key in key_arr:
+        valid_cfar_labels.append(labels[key])
+        valid_cfar_data.append(train_data[key])
+    print("finished process")
 #part a
 validation_size = 10000
 data = data_sets.get("mnist")
@@ -27,32 +56,56 @@ for i in range(len(data.get(fields[2]))):
 #shuffle the data
 random.shuffle(key_arr)
 
-valid_mnist_set = {}
-for i in range(validation_size):
-    key = key_arr[i]
-    print("key: ", i)
-    valid_mnist_set[data.get("training_labels")[key]] = data.get("training_data")[key]
+valid_mnist_labels = []
+valid_mnist_data = []
+
+num_threads = 1
+data_size = validation_size
+mod_size = data_size // num_threads
+threads = []
+for i in range(num_threads):
+    start = i * mod_size
+    end = (i + 1) * mod_size
+    if end > data_size:
+        end = data_size
+    t = Thread(target=make_validation_mnist, args=(key_arr[start : end],),  daemon=True)
+    t.start()
+    threads.append(t)
+[t.join() for t in threads]
+
+    
+
     
 
 #part b
-valid_spam_set = {}
+
 data = data_sets.get("spam")
 key_arr = []
-validation_size = data.get(len("training_labels")) * 0.2
+validation_size = len(data.get("training_labels")) // 2
+labels = data.get("training_labels")
 for i in range(len(data.get("training_labels"))):
     key_arr.append(i)
     print("shuffle: ", i)
 random.shuffle(key_arr)
 
-for i in range(validation_size):
-    key = key_arr[i]
-    print("key: ", i)
-    k = data.get("training_labels")[key]
-    v = data.get("training_data")[key]
-    valid_spam_set[k] = v
+valid_spam_labels = []
+valid_spam_data = []
+
+num_threads = 1
+data_size = int(validation_size)
+mod_size = data_size // num_threads
+threads = []
+for i in range(num_threads):
+    start = i * mod_size
+    end = (i + 1) * mod_size
+    if end > data_size:
+        end = data_size
+    t = Thread(target=make_validation_spam, args=(key_arr[start : end],),  daemon=True)
+    t.start()
+    threads.append(t)
+[t.join() for t in threads]
     
 #part c
-valid_cfar_set = {}
 data = data_sets.get("cifar10")
 key_arr = []
 validation_size = 5000
@@ -62,14 +115,28 @@ for i in range(len(data.get("training_labels"))):
     print("shuffle: ", i)
 random.shuffle(key_arr)
 
-for i in range(validation_size):
-    k = data.get("training_labels")
-    v = data.get("training_data")
-    valid_cfar_set[k] = v
+valid_cfar_labels = []
+valid_cfar_data = []
 
-print("cfar valid set ", len(valid_cfar_set))
-print("spam valid set ", len(valid_spam_set))
-print("mnist valid set ", len(valid_mnist_set))
+num_threads = 1
+data_size = validation_size
+mod_size = data_size // num_threads
+threads = []
+for i in range(num_threads):
+    start = i * mod_size
+    end = (i + 1) * mod_size
+    if end > data_size:
+        end = data_size
+    t = Thread(target=make_validation_cfar, args=(key_arr[start : end],),  daemon=True)
+    t.start()
+    threads.append(t)
+[t.join() for t in threads]
+
+
+print("cfar valid set ", len(valid_cfar_labels), len(valid_cfar_data))
+print("spam valid set ", len(valid_spam_labels), len(valid_spam_data))
+print("mnist valid set ", len(valid_mnist_labels), len(valid_mnist_data))
+print("labels: ", labels)
     
     
 
